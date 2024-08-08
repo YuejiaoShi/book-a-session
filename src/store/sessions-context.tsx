ae5a69feab0fac385e4c315a0202fdd35b28143c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-08 17:04:25 +0200

    create bookSession and cancelSession funcs to handle the sessions modification

--- src/store/sessions-context.tsx
+++ src/store/sessions-context.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useReducer } from "react";
+import { ReactNode, useReducer } from "react";
 
 type Session = {
   id: string;
@@ -14,11 +14,23 @@ type SessionState = {
   upcomingSessions: Session[];
 };
 
-function reducer(state: SessionState, action) {}
+function reducer(state: SessionState, action) {
+  switch (action.type) {
+  }
+}
 function SessionsContextProvider({ children }: { children: ReactNode }) {
   const [sessionsState, dispatch] = useReducer(reducer, {
     upcomingSessions: [],
   });
+
+  function bookSession(session: Session) {
+    dispatch({ type: "BOOK_SESSION", session });
+  }
+
+  function cancelSession(sessionId: string) {
+    dispatch({ type: "CANCEL_SESSION", sessionId });
+  }
+
   return <div>// component content</div>;
 }
 
