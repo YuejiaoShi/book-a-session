d395f9c4002571d82057f2d11feb904eb766b20c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-09 00:42:02 +0200

    return a Modal with a ref

--- src/components/Sessions/UpcomingSessions.tsx
+++ src/components/Sessions/UpcomingSessions.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
+import Modal, { ModalHandle } from "../UI/Modal";
 
 type UpcomingSessionsProps = {
   onClose: () => void;
 };
 
 const UpcomingSessions: React.FC<UpcomingSessionsProps> = ({ onClose }) => {
-  return <div>// component content</div>;
+  const modal = useRef<ModalHandle>(null);
+
+  return (
+    <Modal ref={modal} onClose={onClose}>
+      <h2>Upcoming Sessions</h2>
+    </Modal>
+  );
 };
 
 export default UpcomingSessions;
