ac155ab87cdb4ea01c7dea0b82fdae70ef42f4fe
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-08 16:50:05 +0200

    Add a state to handle isSessionsVisible to be true when Button triggered

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import Button from "../UI/Button";
 
 function Header() {
+  const [isSessionsVisible, setIsSessionsVisible] = useState(false);
+
+  function showSessions() {
+    setIsSessionsVisible(true);
+  }
+
   return (
     <header id="main-header">
       <h1>ReactMentoring</h1>
       <nav>
-        {" "}
         <li>
           <NavLink
             to="/"
@@ -22,6 +29,9 @@ function Header() {
           >
             Browse Sessions
           </NavLink>
+        </li>
+        <li>
+          <Button onClick={showSessions}>Upcoming Sessions</Button>
         </li>
       </nav>
     </header>
