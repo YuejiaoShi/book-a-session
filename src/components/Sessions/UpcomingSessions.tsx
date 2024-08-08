b1d46e17739a4dd40e940789a8af0a0fbd82390b
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-09 00:47:24 +0200

    If it's not hasSessions, render a p text; if hasSessions render a list of upcomingSessions

--- src/components/Sessions/UpcomingSessions.tsx
+++ src/components/Sessions/UpcomingSessions.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import Modal, { ModalHandle } from "../UI/Modal";
+import { useSessionsContext } from "../../store/sessions-context";
+import Button from "../UI/Button";
 
 type UpcomingSessionsProps = {
   onClose: () => void;
@@ -7,6 +9,9 @@ type UpcomingSessionsProps = {
 
 const UpcomingSessions: React.FC<UpcomingSessionsProps> = ({ onClose }) => {
   const modal = useRef<ModalHandle>(null);
+  const sessionsContext = useSessionsContext();
+  const hasSessions = sessionsContext.upcomingSessions.length > 0;
+
   useEffect(() => {
     if (modal.current) {
       modal.current.open();
@@ -16,6 +21,11 @@ const UpcomingSessions: React.FC<UpcomingSessionsProps> = ({ onClose }) => {
   return (
     <Modal ref={modal} onClose={onClose}>
       <h2>Upcoming Sessions</h2>
+      {hasSessions && <ul></ul>}
+      {!hasSessions && <p>No upcoming sessions.</p>}
+      <p className="actions">
+        <Button onClick={onClose}>Close</Button>
+      </p>
     </Modal>
   );
 };
