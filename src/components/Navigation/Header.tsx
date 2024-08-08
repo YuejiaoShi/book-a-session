31dc28ba9a22bc58a85ace553ba4448e0fbd2cfb
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-08 16:38:49 +0200

    add two NavLink to navigate to '/' or '/sessions' pages

--- src/components/Navigation/Header.tsx
+++ src/components/Navigation/Header.tsx
@@ -1,8 +1,29 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
   return (
     <header id="main-header">
       <h1>ReactMentoring</h1>
-      <nav></nav>
+      <nav>
+        {" "}
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "")}
+            end
+          >
+            Our Mission
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/sessions"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Browse Sessions
+          </NavLink>
+        </li>
+      </nav>
     </header>
   );
 }
