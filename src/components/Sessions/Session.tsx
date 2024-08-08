481a50d4381855c8d18b01c9c05e84221124a645
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-08 22:01:31 +0200

    fill session with image,title, summary text, and a LearnMore Button to link to the details with the id of session

--- src/components/Sessions/Session.tsx
+++ src/components/Sessions/Session.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../UI/Button";
 
 type SessionItemProps = {
   id: string;
@@ -8,7 +9,20 @@ type SessionItemProps = {
 };
 
 const Session: React.FC<SessionItemProps> = ({ id, title, summary, image }) => {
-  return <article className="session-item"></article>;
+  return (
+    <article className="session-item">
+      <img src={image} alt={title} />
+      <div className="session-data">
+        <div>
+          <h3>{title}</h3>
+          <p>{summary}</p>
+        </div>
+        <p className="actions">
+          <Button to={id}>Learn More</Button>
+        </p>
+      </div>
+    </article>
+  );
 };
 
 export default Session;
